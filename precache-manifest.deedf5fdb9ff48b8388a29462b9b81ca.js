938be77bbc5c88dacf3be7a674a389621ce51daf
emireken <emirgokberk.eken@gmail.com>
2021-06-28 22:39:29 -0400

    Updates

---
+++ precache-manifest.deedf5fdb9ff48b8388a29462b9b81ca.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efdc66515efbad9890eeb5073207388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20b1d249f1eab1d36abc",
+    "url": "/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "4f7b19f4f91c4ecb1e84",
+    "url": "/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "0397663d6e59d5370625",
+    "url": "/static/css/main.09c12c85.chunk.css"
+  },
+  {
+    "revision": "7fb654a926e5fc657c41",
+    "url": "/static/js/2.ebfbc486.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.ebfbc486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b1d249f1eab1d36abc",
+    "url": "/static/js/3.48dbe6a3.chunk.js"
+  },
+  {
+    "revision": "4f7b19f4f91c4ecb1e84",
+    "url": "/static/js/4.36667f7b.chunk.js"
+  },
+  {
+    "revision": "0397663d6e59d5370625",
+    "url": "/static/js/main.9f3597f4.chunk.js"
+  },
+  {
+    "revision": "1dc9ce861689ff3795d6",
+    "url": "/static/js/runtime-main.2f72a5f8.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "7bfe715d40b0f145824541910b692772",
+    "url": "/static/media/acibadem_logo.7bfe715d.png"
+  },
+  {
+    "revision": "6c0d146bd0c75847302547c427a9bf80",
+    "url": "/static/media/atu_logo.6c0d146b.png"
+  },
+  {
+    "revision": "608563a12d5073879888e7e138b2a2ed",
+    "url": "/static/media/azak_logo.608563a1.png"
+  },
+  {
+    "revision": "86053b6f5b7ba6a6cbad98a78e3bcbf6",
+    "url": "/static/media/bau_logo.86053b6f.png"
+  },
+  {
+    "revision": "d141a8e321e3552f9693af900765074c",
+    "url": "/static/media/cadio_logo.d141a8e3.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e9771cef765fa9a76a5cc76a297e9f4b",
+    "url": "/static/media/fya_logo.e9771cef.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "1e8ae92025500199c20a8901eed405f6",
+    "url": "/static/media/stellantis_logo.1e8ae920.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
