92baee5fa183a0dd2e0e11a7153601851cdd9096
emireken <emirgokberk.eken@gmail.com>
2021-07-07 14:28:16 -0400

    android update

---
+++ build/precache-manifest.6354afadf9cb7f1842d1f37fbad3d040.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ae47555e5420c6766bb0175e25965f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60e3eb98f1c7e30c7882",
+    "url": "/portfolio/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "5e270b83435c2a5582e3",
+    "url": "/portfolio/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "2ee38237ef5504a9872d",
+    "url": "/portfolio/static/css/main.a411e791.chunk.css"
+  },
+  {
+    "revision": "147d37d79429011c4a19",
+    "url": "/portfolio/static/js/2.a1f0348a.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.a1f0348a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e3eb98f1c7e30c7882",
+    "url": "/portfolio/static/js/3.791ba641.chunk.js"
+  },
+  {
+    "revision": "5e270b83435c2a5582e3",
+    "url": "/portfolio/static/js/4.b8aa66ad.chunk.js"
+  },
+  {
+    "revision": "2ee38237ef5504a9872d",
+    "url": "/portfolio/static/js/main.d018140f.chunk.js"
+  },
+  {
+    "revision": "72e49d04117eab05fd1a",
+    "url": "/portfolio/static/js/runtime-main.c0014281.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/portfolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "bb58e698ab106285f62e93d1d2a9d6df",
+    "url": "/portfolio/static/media/acibadem_logo.bb58e698.png"
+  },
+  {
+    "revision": "82a5ad21bb9a97e0421f3f578dabfe22",
+    "url": "/portfolio/static/media/astore_logo.82a5ad21.png"
+  },
+  {
+    "revision": "7cafcfb9ac58e22a72d3d3d5646d43e1",
+    "url": "/portfolio/static/media/atu_logo.7cafcfb9.png"
+  },
+  {
+    "revision": "d05eae31fe5bbb7d4dbb2df1482396d4",
+    "url": "/portfolio/static/media/azak_logo.d05eae31.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "995a27de1280c502b2a69f9f1b10aa17",
+    "url": "/portfolio/static/media/fya_logo.995a27de.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
